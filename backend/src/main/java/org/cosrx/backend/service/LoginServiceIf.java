ae86c6f286eb9705fd15d6f1ecbfb3931f9ae8ee
jy <juyoun919@gmail.com>
2025-04-10 17:33:21 +0900

    로그인 기능 구현(에러 처리 X)

--- backend/src/main/java/org/cosrx/backend/service/LoginServiceIf.java
+++ backend/src/main/java/org/cosrx/backend/service/LoginServiceIf.java
@@ -4,5 +4,5 @@ import org.cosrx.backend.dto.LoginDTO;
 import org.cosrx.backend.dto.UserDTO;
 
 public interface LoginServiceIf {
-    public UserDTO login(LoginDTO loginDTO);
+    public UserDTO login_info(LoginDTO loginDTO);
 }
--- backend/src/main/java/org/cosrx/backend/service/LoginServiceImpl.java
+++ backend/src/main/java/org/cosrx/backend/service/LoginServiceImpl.java
@@ -1,24 +1,29 @@
 package org.cosrx.backend.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.cosrx.backend.domain.UserEntity;
 import org.cosrx.backend.dto.LoginDTO;
 import org.cosrx.backend.dto.UserDTO;
 import org.cosrx.backend.repository.LoginRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Log4j2
+@Service
+@RequiredArgsConstructor
 public class LoginServiceImpl implements LoginServiceIf {
-//    private final ModelMapper modelMapper;
-//    private final LoginRepository loginRepository;
+    private final ModelMapper modelMapper;
+    private final LoginRepository loginRepository;
 //
     @Override
-    public UserDTO login(LoginDTO loginDTO) {
-//        Optional<UserEntity> result = loginRepository.findByUserId(loginDTO.getUserId());
-//        UserEntity user = result.orElse(null);
-//        UserDTO userDTO = modelMapper.map(user, UserDTO.class);
-//        return userDTO;
-        return null;
+    public UserDTO login_info(LoginDTO loginDTO) {
+        Optional<UserEntity> result = loginRepository.findByUserId(loginDTO.getUserId());
+        UserEntity user = result.orElse(null);
+        UserDTO userDTO = modelMapper.map(user, UserDTO.class);
+        return userDTO;
     }
 
 
