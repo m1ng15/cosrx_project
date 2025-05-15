4edef0398e10759be7449f6149542cca4a306a9a
min <www.ming115@gmail.com>
2025-05-15 16:57:44 +0900

    common php 생성 및 php 변환

---
+++ frontend/common/headerTop.php
@@ -0,0 +1,11 @@
+<ul class="hd_message">
+    <ul class="message_list">
+        <li><a href="#">공식몰 회원가입 시, 무료배송 쿠폰 증정</a></li>
+    </ul>
+</ul>
+<div class="page_setting">
+    <a href="#">고객센터</a>
+    <span class="line"></span>
+    <a href="#">언어설정</a>
+</div>
+<button type="button" id="close_btn"><img src="images/close.png" alt=""></button>
---
+++ frontend/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ko">
+<?php $pageTitle = "COSRX 메인페이지 | 정민재"; ?>
+<head>
+    <?php include("common/head.php"); ?>
+    <link rel="stylesheet" href="styles/login.css">
+</head>
+<body>
+    <div class="hd_top">
+        <?php include("common/headerTop.php"); ?>
+    </div>
+    <header>
+        <?php include("common/header.php"); ?>
+    </header>
+    <main>
+        
+    </main>
+    <footer>
+        <?php include("common/footer.php"); ?>
+    </footer>
+</body>
+</html>
