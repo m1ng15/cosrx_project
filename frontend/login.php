1550e871ba25f582d172fd2887467975d7cd88a2
min <www.ming115@gmail.com>
2025-06-04 19:47:25 +0900

    6월 4일 업로드

--- frontend/login.php
+++ frontend/login.php
@@ -3,6 +3,8 @@
 <?php $pageTitle = "COSRX 로그인/회원가입"; ?>
 <head>
     <?php include("common/head.php"); ?>
+    <!-- 로그인 페이지 별도 js & css -->
+    <script src="script/login.js" defer></script>
     <link rel="stylesheet" href="styles/login.css">
 </head>
 <body>
@@ -12,42 +14,75 @@
     <header>
         <?php include("common/header.php"); ?>
     </header>
-    <main class="login_wrap">
-        <section class="login_box">
-            <h2>로그인</h2>
-            <p>아이디와 비밀번호 입력하기 귀찮으시죠?<br>기존 SNS 계정으로 1초 만에 로그인 / 회원가입 하세요.</p>
-            <div class="sns_login">
-                <!-- SNS 아이콘 -->
-                <img src="#" alt="kakao">
-                <img src="#" alt="naver">
-                <img src="#" alt="google">
-                <img src="#" alt="apple">
-            </div>
-            <form action="#" method="post" class="loginFrm">
-                <label for="id">아이디</label>
-                <input type="text" id="id" name="id" maxlength="20"  placeholder="아이디">
-                
-                <label for="pw">비밀번호</label>
-                <input type="password" id="pw" name="pw" placeholder="비밀번호">
-
-                <div class="login_options">
-                    <label><input type="checkbox" checked> 아이디 저장</label>
-                    <label><input type="checkbox"> 자동 로그인</label>
-                    <a href="#">로그인/비밀번호 찾기</a>
+    <main>
+        <div class="login_wrap">
+            <section class="login_box">
+                <div class="title">
+                    <h2>로그인</h2>
+                    <p>아이디와 비밀번호 입력하기 귀찮으시죠?<br>기존 SNS 계정으로 1초 만에 로그인 / 회원가입 하세요.</p>
                 </div>
-                
-                <button type="submit" class="btn_login">로그인</button>
-                <button type="button" class="btn_signup" disabled>회원가입</button>
-            </form>
-            <div class="guest_order">
-                <a href="#">비회원 주문조회</a>
-            </div>
-        </section>
-        <aside class="membership_bnr">
-            <h3>멤버십 혜택</h3>
-            <p>코스알엑스 공식몰에서만 만나볼 수 있는 혜택!</p>
-            <img src="#" alt="첫 회원가입 혜택 이미지">
-        </aside>
+                <div class="sns_icons">
+                    <a href="#" class="icon kakao" title="카카오톡"></a>
+                    <a href="#" class="icon naver" title="네이버"></a>
+                    <a href="#" class="icon google" title="구글"></a>
+                    <a href="#" class="icon apple" title="애플"></a>
+                </div>
+                <form action="#" method="post" class="loginFrm">
+                    <div class="id_group">
+                        <div class="id_input input_group">
+                            <input type="text" id="id" name="id" maxlength="20" placeholder="" required>
+                            <label for="id">아이디</label>
+                            <button type="button" class="x_btn" data-target="id"><img src="./images/close_bk.png" alt="아이디 지우기"></button>
+                        </div>
+                        <p>아이디를 입력해주세요</p>
+                    </div>
+                    <div class="pw_group">
+                        <div class="pw_input input_group">
+                            <input type="password" id="pw" name="pw" placeholder="" required>
+                            <label for="pw">비밀번호</label>
+                            <button type="button" id="togglePw" class="eye_btn" disabled><img src="./images/eye_off.png" alt=""></button>
+                            <button type="button" class="x_btn" data-target="pw"><img src="./images/close_bk.png" alt="비밀번호 지우기"></button>
+                        </div>
+                        <p>비밀번호를 입력해주세요</p>
+                    </div>    
+                    <div class="login_options">
+                        <div class="checkbox_group">
+                            <label class="save_id">
+                                <input type="checkbox" checked>
+                                <span class="checkbox_icon"></span>
+                                아이디 저장
+                            </label>
+                            <label class="auto_login">
+                                <input type="checkbox">
+                                <span class="checkbox_icon"></span>
+                                자동 로그인
+                            </label>
+                        </div>
+                        <a href="#">로그인/비밀번호 찾기</a>
+                    </div>
+                    <div class="form_btn">
+                        <button type="submit" class="btn_login">로그인</button>
+                        <button type="button" class="btn_signup">
+                            회원가입
+                            <span class="border left"></span>
+                            <span class="border bottom"></span>
+                        </button>
+                    </div>
+                </form>
+                <div class="guest_order">
+                    <a href="#">비회원 주문조회</a>
+                </div>
+            </section>
+            <aside>
+                <div class="membership_bnr">
+                    <div class="title">
+                        <h2>멤버십 혜택</h2>
+                        <p>코스알엑스 공식몰에서만 만나볼 수 있는 혜택!</p>
+                    </div>
+                    <div class="bnr_image"></div>
+                </div>
+            </aside>
+        </div>
     </main>
     <footer>
         <?php include("common/footer.php"); ?>
